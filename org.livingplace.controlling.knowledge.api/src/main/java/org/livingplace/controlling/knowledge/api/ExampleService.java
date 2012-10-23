1d8fe9a6f7dd7e539c78316fb4438d21dbb067c9
Kjellski <otto.kjell@gmail.com>
2012-10-23 10:23:47 +0200

    added knowledge project

---
+++ org.livingplace.controlling.knowledge.api/src/main/java/org/livingplace/controlling/knowledge/api/ExampleService.java
@@ -0,0 +1,12 @@
+package org.livingplace.controlling.knowledge.api;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+

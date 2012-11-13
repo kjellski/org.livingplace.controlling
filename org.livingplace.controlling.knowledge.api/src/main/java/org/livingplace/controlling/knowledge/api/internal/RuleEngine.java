6ba977f832abafff772c83f647f4c0827ae861ea
Kjellski <otto.kjell@gmail.com>
2012-11-13 10:09:34 +0100

    Repush before knowledge api work starts for real.

--- org.livingplace.controlling.knowledge.api/src/main/java/org/livingplace/controlling/knowledge/api/internal/RuleEngine.java
+++ org.livingplace.controlling.knowledge.api/src/main/java/org/livingplace/controlling/knowledge/api/internal/RuleEngine.java
@@ -1,17 +1,18 @@
 package org.livingplace.controlling.knowledge.api.internal;
 
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.*;
 import org.livingplace.controlling.actions.registry.api.IActionRegistryFactory;
 import org.livingplace.controlling.informations.registry.api.IInformationRegistryFactory;
 import org.livingplace.controlling.knowledge.api.IRuleEngine;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.log.LogService;
 
+import java.util.Dictionary;
+
 @Component(immediate = true)
-public class RuleEngine implements IRuleEngine {
+public class RuleEngine implements IRuleEngine, ManagedService {
 
   @Reference
   IActionRegistryFactory actionRegistryFactory;
@@ -24,31 +25,39 @@ public class RuleEngine implements IRuleEngine {
 
   DroolsManager engineManager;
 
+  @Property(value="default value given by annotation")
+  static final String CONSTANT_NAME = "property.name";
+
   @Activate
   public void start(){
-    System.out.println("\nSTARTING RULEENGINE...");
+    log.log(LogService.LOG_INFO, "\nSTARTING RULEENGINE...");
     init();
-    System.out.println();
-    System.out.println("STARTED RULEENGINE.");
-    System.out.println();
+    log.log(LogService.LOG_INFO, "STARTED RULEENGINE.");
     log.log(LogService.LOG_INFO, RuleEngine.class.getName() + " started.");
+  }
+
+  @Override
+  public void updated(Dictionary<String, ?> stringDictionary) throws ConfigurationException {
+
+  }
+
+  @Deactivate
+  public void stop(){
+    engineManager.interrupt();
+    log.log(LogService.LOG_INFO, "STOPPED RULEENGINE!");
+    log.log(LogService.LOG_INFO, RuleEngine.class.getName() + " stopped.");
   }
 
   private void init() {
     try {
-      System.out.println();
-      System.out.println("Registered Actions:      " + actionRegistryFactory.getInstance().getAllRegistered());
-      System.out.println("Registered Informations: " + informationRegistryFactory.getInstance().getAllRegistered());
+      log.log(LogService.LOG_INFO, "Registered Actions:      " + actionRegistryFactory.getInstance().getAllRegistered());
+      log.log(LogService.LOG_INFO, "Registered Informations: " + informationRegistryFactory.getInstance().getAllRegistered());
       engineManager = new DroolsManager(log);
     } catch (Exception e) {
       e.printStackTrace(System.out);
     }
   }
 
-  @Deactivate
-  public void stop(){
-    engineManager.interrupt();
-    System.out.println("STOPPED RULEENGINE!");
-    log.log(LogService.LOG_INFO, RuleEngine.class.getName() + " stopped.");
-  }
+
+
 }

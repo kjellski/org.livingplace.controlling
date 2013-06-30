0443be1c4ac2508a857beab97c58e2f471e1b905
kjellski <otto.kjell@gmail.com>
2013-06-30 18:49:42 +0200

    added activemq messaging wrapper dependency in a working state. can be much further improved...

--- org.livingplace.controlling.informations.sensors.position/src/main/java/org/livingplace/controlling/informations/sensors/position/PositionSensor.java
+++ org.livingplace.controlling.informations.sensors.position/src/main/java/org/livingplace/controlling/informations/sensors/position/PositionSensor.java
@@ -12,8 +12,8 @@ import org.livingplace.controlling.informations.registry.api.IInformationRegistr
 import org.livingplace.controlling.informations.sensors.position.internal.Position;
 import org.livingplace.controlling.informations.sensors.position.internal.PositionInformation;
 import org.livingplace.controlling.informations.sensors.position.internal.UbisensePositionMessage;
-import org.livingplace.messaging.activemq.impl.internal.ConnectionSettings;
-import org.livingplace.messaging.activemq.impl.internal.LPSubscriber;
+import org.livingplace.messaging.activemq.impl.tmp.LPConnectionSettings;
+import org.livingplace.messaging.activemq.impl.tmp.LPSubscriber;
 
 import javax.jms.JMSException;
 import java.util.ArrayList;
@@ -24,56 +24,56 @@ import java.util.Timer;
 @Service
 public class PositionSensor extends Sensor implements ISensor {
 
-  List<IInformationListener> listeners = new ArrayList<IInformationListener>();
-  Timer timer = new Timer();
-  private static Logger logger = Logger.getLogger(PositionSensor.class);
+    List<IInformationListener> listeners = new ArrayList<IInformationListener>();
+    Timer timer = new Timer();
+    private static Logger logger = Logger.getLogger(PositionSensor.class);
 
-  @Reference
-  protected IInformationRegistryFactory informationRegistryFactory;
+    @Reference
+    protected IInformationRegistryFactory informationRegistryFactory;
 
-  public PositionSensor() {
-    super(new SensorQualifier("position", "positioner", "1.0"));
-  }
-
-  @Activate
-  void start(){
-    logger.info("PositionSensor started");
-
-    informations.add(new PositionInformation(this, new Position()));
-
-    for (IInformation information : informations) {
-      listeners.add(informationRegistryFactory.getInstance().registerOnListener(information));
+    public PositionSensor() {
+        super(new SensorQualifier("position", "positioner", "1.0"));
     }
-  }
 
-  {
-    ConnectionSettings cs = new ConnectionSettings();
-    cs.amq_ip = "172.16.0.200";
-    cs.mongo_ip = "172.16.0.200";
+    @Activate
+    void start() {
+        logger.info("PositionSensor started");
 
-    try {
-      LPSubscriber subscriber = new LPSubscriber("UbisenseTracking", cs);
+        informations.add(new PositionInformation(this, new Position()));
 
-      for (;;) {
-        Gson gson = new Gson();
-        UbisensePositionMessage msg = gson.fromJson(subscriber.subscribeBlocking(), UbisensePositionMessage.class);
-        for (IInformationListener listener : listeners) {
-          listener.sensedInformation(new PositionInformation(this, msg.getNewPosition()));
+        for (IInformation information : informations) {
+            listeners.add(informationRegistryFactory.getInstance().registerOnListener(information));
         }
-      }
-    } catch (JMSException e) {
-      logger.error(this.getQualifier() + ": error while getting positions from Topic \"UbisenseTracking\"", e);
-      e.printStackTrace();
-    }
-  }
 
-  @Deactivate
-  void stop(){
-    for (IInformation information : informations) {
-      informationRegistryFactory.getInstance().unregister(information);
+        LPConnectionSettings cs = new LPConnectionSettings();
+//        cs.setActiveMQIp("172.16.0.200");
+//        cs.setMongoDBIp("172.16.0.200");
+
+        try {
+            LPSubscriber subscriber = new LPSubscriber("UbisenseTracking", cs);
+
+            for (; ; ) {
+                Gson gson = new Gson();
+                String msg = subscriber.subscribeBlocking();
+                logger.info("Sensed UbisenseTracking information: " + msg);
+                UbisensePositionMessage pos = gson.fromJson(msg, UbisensePositionMessage.class);
+                for (IInformationListener listener : listeners) {
+                    listener.sensedInformation(new PositionInformation(this, pos.getNewPosition()));
+                }
+            }
+        } catch (JMSException e) {
+            logger.error(this.getQualifier() + ": error while getting positions from Topic \"UbisenseTracking\"", e);
+            e.printStackTrace();
+        }
     }
-    timer.cancel();
-    logger.info("PositionSensor stopped");
-  }
+
+    @Deactivate
+    void stop() {
+        for (IInformation information : informations) {
+            informationRegistryFactory.getInstance().unregister(information);
+        }
+        timer.cancel();
+        logger.info("PositionSensor stopped");
+    }
 
 }
